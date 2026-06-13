e9882ca8f1bccc06ffeb60acb95c20cd83deb175
ArashDev <arashwower@yahoo.com>
2026-06-13 22:58:37 +0330

    deploy changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,6 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = withNextIntl(nextConfig);
 
-export default nextConfig;
